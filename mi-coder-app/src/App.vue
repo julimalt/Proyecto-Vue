0fa6c7644668cabee5b11762ae87f840be90c613
julimalt <julietamaalt@gmail.com>
2021-12-01 23:39:22 -0300

    Vuetify

--- mi-coder-app/src/App.vue
+++ mi-coder-app/src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-simple-table dark>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            product
+          </th>
+          <th class="text-left">
+            price
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in desserts"
+          :key="item.product"
+        >
+          <td>{{ item.product }}</td>
+          <td>{{ item.price }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data () {
+      return {
+        desserts: [
+          {
+            product: 'Frozen Yogurt',
+            price: 159,
+          },
+          {
+            product: 'Ice cream sandwich',
+            price: 237,
+          },
+          {
+            product: 'Eclair',
+            price: 262,
+          },
+          {
+            product: 'Cupcake',
+            price: 305,
+          },
+          {
+            product: 'Gingerbread',
+            price: 356,
+          },
+          {
+            product: 'Jelly bean',
+            price: 375,
+          },
+          {
+            product: 'Lollipop',
+            price: 392,
+          },
+          {
+            product: 'Honeycomb',
+            price: 408,
+          },
+          {
+            product: 'Donut',
+            price: 452,
+          },
+          {
+            product: 'KitKat',
+            price: 518,
+          },
+        ],
+      }
+    },
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
